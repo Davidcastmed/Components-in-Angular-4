4ea5c14942f60aacbe1b1da8f7b786e648bb73c3
unknown <qxuser23@quixotic-pc23.trisourcebpo.local>
2017-10-26 18:08:59 -0600

    first commit

---
+++ src/app/timer/timer.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit, OnDestroy, Input, Output, EventEmitter } from '@angular/core';
+import { TimerService } from 'app/timer/timer.service';
+import { Subscription } from 'rxjs/Subscription';
+
+@Component({
+  selector: 'app-timer',
+  templateUrl: './timer.component.html',
+  styleUrls: ['./timer.component.scss'],
+  providers:[TimerService]
+})
+
+
+export class TimerComponent implements OnInit, OnDestroy {
+
+  @Output() onComplete = new EventEmitter<void>();
+  @Input() init:number = 20;
+  private countdownEndSubscription:Subscription=null;
+
+
+
+  constructor(public timer:TimerService) { }
+
+  ngOnInit(): void {
+    
+    this.timer.restartCountdown(this.init);
+
+    this.countdownEndSubscription= this.timer.countdownEnd$.subscribe(()=>{
+      this.onComplete.emit();
+    })
+  }
+
+  ngOnDestroy(){
+   this.timer.destroy();
+   this.countdownEndSubscription.unsubscribe();
+  }
+
+}
